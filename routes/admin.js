b3b5965e787ec38a5bd6489d278d90ac809e373e
wangzhenxin <821622223@qq.com>
2018-06-29 16:27:16 +0800

    获取用户列表

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,47 @@
 var express = require('express');
 var router = express.Router();
+var Model = require('../models/User');
+router.use(function(req, res, next) {
+    if (!req.userInfo.isAdmin) {
+        //如果当前用户是非管理员
+        res.send('对不起，只有管理员才可以进入后台管理');
+        return;
+    }
+    next();
+});
 
-router.get('/user', function(req, res, next) {
-    res.send('User');
+/**
+ *
+ * 首页
+ */
+router.get('/', function(req, res, next) {
+    res.render('admin/index', {
+        userInfo: req.userInfo
+    });
+});
+/**
+ *
+ * 用户
+ */
+router.get('/user', function(req, res) {
+    /*
+    * 从数据库中读取所有的用户数据
+    *
+    * limit(Number) : 限制获取的数据条数
+    *
+    * skip(2) : 忽略数据的条数
+    *
+    * 每页显示2条
+    * 1 : 1-2 skip:0 -> (当前页-1) * limit
+    * 2 : 3-4 skip:2
+    * */
+    Model.find().then(function(users) {
+        res.render('admin/user_index', {
+            userInfo: req.userInfo,
+            users: users
+        });
+    })
+
 });
 
 module.exports =  router;
