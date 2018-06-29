d982477e9b186e30b3dc43cbbd597d25bf276ec2
wangzhenxin <821622223@qq.com>
2018-06-29 17:04:40 +0800

    用户列表分页功能实现

--- routes/admin.js
+++ routes/admin.js
@@ -35,12 +35,33 @@ router.get('/user', function(req, res) {
     * 1 : 1-2 skip:0 -> (当前页-1) * limit
     * 2 : 3-4 skip:2
     * */
-    Model.find().then(function(users) {
-        res.render('admin/user_index', {
-            userInfo: req.userInfo,
-            users: users
-        });
+
+    var page = Number(req.query.page || 1);
+    var limit = 2;
+    var pages = 0;
+    Model.count().then(function(count){
+        console.log(count)
+        //计算总页数
+        pages = Math.ceil(count / limit);
+        //取值不能超过pages
+        page = Math.min(page, pages);
+        //取值不能小于1
+        page = Math.max(page, 1);
+
+        var skip = (page-1)*limit;
+
+        Model.find().limit(limit).skip(skip).then(function(users) {
+            res.render('admin/user_index', {
+                userInfo: req.userInfo,
+                users: users,
+                count: count,
+                limit: limit,
+                pages: pages,
+                page: page
+            });
+        })
     })
+
 
 });
 
