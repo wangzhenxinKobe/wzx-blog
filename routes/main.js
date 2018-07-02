3c60482e54d68491247be58c1d44308b44c6ca76
wangzhenxin <821622223@qq.com>
2018-07-02 16:20:44 +0800

    评论分页

--- routes/main.js
+++ routes/main.js
@@ -3,31 +3,43 @@ var router = express.Router();
 var Category = require('../models/Category');
 var Content = require('../models/Content');
 
+
+var data;
+
+/*
+* 处理通用的数据
+* */
+router.use(function (req, res, next) {
+    data = {
+        userInfo: req.userInfo,
+        categories: []
+    }
+
+    Category.find().then(function(categories) {
+        data.categories = categories;
+        next();
+    });
+});
+
+
+
+
 /*
 *首页
 * */
 router.get('/', function(req, res, next) {
 
-    var data = {
-        userInfo: req.userInfo,
-        category: req.query.category || '',
-        categories: [],
-        page: Number(req.query.page || 1),
-        count: 0,
-        limit: 4,
-        pagesL: 0
-    }
+    data.category = req.query.category || '';
+    data.count = 0;
+    data.page = Number(req.query.page || 1);
+    data.limit = 10;
+    data.pages = 0;
 
     var where = {};
     if(data.category) {
         where.category = data.category
-    }
-
-    //读取所有分类信息
-    Category.find().then(function(categories) {
-        data.categories = categories;
-        return Content.where(where).count();
-    }).then(function(count) {
+    };
+    Category.find().count().then().then(function(count) {
 
         data.count = count;
         //计算总页数
@@ -45,6 +57,24 @@ router.get('/', function(req, res, next) {
         data.contents = contents;
         console.log(contents)
         res.render('main/index',data);
+    });
+
+});
+
+
+router.get('/view', function (req, res){
+
+    var contentId = req.query.contentid || '';
+
+    Content.findOne({
+        _id: contentId
+    }).then(function (content) {
+        data.content = content;
+
+        content.views++;
+        content.save();
+
+        res.render('main/view', data);
     });
 
 });
